aa2dc2a45c0ff5a0fe0f6089f790d9a330fe080a
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-03 17:16:33 -0700

    Add prompt to reset the sketchpad

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function createBox() {
+function createPixel() {
   const div = document.createElement("div");
   div.classList.add("pixel");
+  div.addEventListener("mouseenter", (event) => {
+    event.target.style.backgroundColor = "black";
+  });
   return div;
 }
 
@@ -9,27 +12,42 @@ function createRow(parent, length) {
   row.classList.add("row");
   row.style.display = "flex";
   for (let i = 0; i < length; i++) {
-    const square = createBox();
+    const square = createPixel();
     row.appendChild(square);
   }
   return row;
 }
 
-function createGrid(parent, dim = 16) {
+function createGrid(parent, size = 16) {
   parent.style.display = "flex";
   parent.style.flexDirection = "column";
-  for (let i = 0; i < dim; i++) {
-    const row = createRow(parent, dim);
+  for (let i = 0; i < size; i++) {
+    const row = createRow(parent, size);
     parent.appendChild(row);
   }
+}
+
+function promptForGridSize() {
+  let gridSize = Number(prompt("Enter grid size (Max: 100)"));
+  if (isNaN(gridSize)) {
+    gridSize = 24;
+  } else if (gridSize > 100) {
+    gridSize = 100;
+  } else if (gridSize < 4) {
+    gridSize = 4;
+  }
+  return gridSize;
+}
+
+function resetGrid() {
+  size = promptForGridSize();
+  const sketchpad = document.querySelector(".sketchpad");
+  sketchpad.replaceChildren();
+  createGrid(sketchpad, size);
 }
 
 const sketchpad = document.querySelector(".sketchpad");
 createGrid(sketchpad);
 
-pixels = document.querySelectorAll(".pixel");
-for (const pixel of pixels) {
-  pixel.addEventListener("mouseenter", (event) => {
-    event.target.style.backgroundColor = "black";
-  });
-}
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", resetGrid);
